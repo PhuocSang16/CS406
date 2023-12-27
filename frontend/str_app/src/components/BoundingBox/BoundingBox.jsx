03f346d7d92615c9dec911b2b7291ac934f65082
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2023-12-28 00:22:48 +0700

    Update BoundingBox.jsx
    fix bug ui

--- frontend/str_app/src/components/BoundingBox/BoundingBox.jsx
+++ frontend/str_app/src/components/BoundingBox/BoundingBox.jsx
@@ -6,28 +6,12 @@ import { AppContext } from '../../contexts/AppContext';
 import cx from '../ClassUtils/ClassUtils';
 
 
-function BoundingBox({ index, polygon }) {
-    const { imageWidth, imageHeight, imageRef, resultFromApi , scaleX, scaleY} = useContext(ImageContext);
+function BoundingBox({ index, polygon, rawWidth, rawHeight }) {
+    const { imageWidth, imageHeight, resultFromApi } = useContext(ImageContext);
     const { setSelectedBbox, setSelectedImageName, setSelectedImageWidth, setSelectedImageHeight, setSelectedBboxConfDetScore, setSelectedBboxConfRecScore, setSelectedBboxLabel } = useContext(AppContext);
-    console.log(scaleX, scaleY)
-    // let scaleX = 1
-    // let scaleY = 1
-    
-    // if (imageWidth)
-    //     if (imageHeight === 271.5)
-    //     {
-    //         scaleX = imageWidth ? 362 / 800 :1//imageWidth / imageRef.current.width : 1;
-    //         scaleY = imageHeight ? 271.5 / 600: 1 //imageHeight / imageRef.current.height: 1;
-    //         console.log(scaleX, scaleY)
-    //     }
-    //     else
-    //     {
-    //         scaleX = imageWidth / imageRef.current.width
-    //         scaleY = imageHeight / imageRef.current.height
-    //     }
-    // if (imageWidth) 
-    //     console.log(imageWidth, imageRef.current.width, imageHeight, imageRef.current.height)
 
+    const scaleX = imageWidth / rawWidth;
+    const scaleY = imageHeight / rawHeight;
     const scaledPoints = polygon.map(([x, y]) => [x * scaleX, y * scaleY]);
     const pointsString = scaledPoints.map(([x, y]) => `${x},${y}`).join(' ');
 
